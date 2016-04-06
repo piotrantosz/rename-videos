43900237f44bb77cd1e9f0c8ae8e1a62efe062ba
Piotr <piotrantosz@protonmail.ch>
2016-04-06 19:50:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rename filenames of movies etc.
+
+Simple script to sort all your files using guessit lib (https://github.com/guessit-io/guessit)
+
+### Installation
+`pip install guessit`
+`python rename.py`
+
+### Use
+Just write path to your movie directory. Script will change also subdirectories with files.
+
+### Output
+New filename will contain title + season + episode (if exists)
---
+++ rename.py
@@ -0,0 +1,24 @@
+from guessit import guessit
+import os.path
+
+mypath = raw_input("Enter the path of your movies catalog: ")
+
+for root, subdirs, files in os.walk(mypath):
+    for f in files:
+        guess_file = guessit(f)
+        re_f = guess_file['title'].title()
+        if 'season' in guess_file:
+            re_f += ' - season ' + str(guess_file['season'])
+            if 'episode' in guess_file:
+                re_f += ' episode ' + str(guess_file['episode'])
+        os.rename(os.path.join(root, f), os.path.join(root, re_f))
+
+    for d in subdirs:
+        guess_dir = guessit(d)
+        re_d = guess_dir['title'].title()
+        if 'season' in guess_dir:
+            re_d += ' - season ' + str(guess_dir['season'])
+            if 'episode' in guess_dir:
+                re_d += ' episode ' + str(guess_dir['episode'])
+        os.rename(os.path.join(root, d), os.path.join(root, re_d))
+
